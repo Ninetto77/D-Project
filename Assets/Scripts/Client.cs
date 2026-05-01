52048a82cdc3a23e6a6e79f6970c1ca81cbf7c44
Ninetto77 <ndsingkh@stud.eltech.ru>
2026-05-01 18:20:42 +0300

    fix: clients messages

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -1,6 +1,7 @@
 using Mirror;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace DProjectMirror
@@ -16,38 +17,61 @@ namespace DProjectMirror
 		[Inject]
 		private Server server_;
 
-		public override void OnStartLocalPlayer()
-		{
-			Debug.Log("OnStartLocalPlayer");
+		// Только для теста: создаем текст для дебага билда
+		[SerializeField] private Text debug_;
 
-			if (isLocalPlayer)
+		/// <summary>
+		/// Функция активации клиента.
+		/// </summary>
+		public override void OnStartClient()
+		{
+			base.OnStartClient();
+			Debug.Log("OnStartClient");
+			if (NetworkClient.active && isLocalPlayer)
 			{
+				Debug.Log("active");
 				// Регистрируем обработчик сообщений на получение HelloMessage (ответ от сервера)
+				CmdSubscribe();
 				NetworkClient.RegisterHandler<HelloMessage>(OnHelloMessageReceived);
-				Subscribe();
 			}
 		}
-		
+
+		/// <summary>
+		/// Функция остановки клиента.
+		/// </summary>
+		public override void OnStopClient()
+		{
+			base.OnStopClient();
+			NetworkClient.UnregisterHandler<HelloMessage>();
+		}
+
 		/// <summary>
 		/// Функция ответ от сервера на подписку клиента.
 		/// </summary>
 		/// <param name="msg"> Тип подписки. </param>
 		private void OnHelloMessageReceived(HelloMessage msg)
 		{
+			SetText($"Клиент получил сообщение: {msg.msg}");
 			Debug.Log($"Клиент получил сообщение: {msg.msg}");
 		}
 
 		/// <summary>
 		/// Клиент отправляет запрос на подключение подписки на сообщение конкретного типа.
 		/// </summary>
-		[Command]
-		void Subscribe()
+		//[Command]
+		private void CmdSubscribe()
 		{
-			if (server_)
-			{
-				Debug.Log("server is ok");				
-				server_.CmdSubscribe(connectionToClient);
-			}
+			Debug.Log("server ");
+			//if (server_)
+			//{
+			//	Debug.Log("server is ok");				
+			//	server_.CmdSubscribe(connectionToClient);
+			//}
+		}
+
+		private void SetText(string s)
+		{
+			debug_.text = s;
 		}
 	}
 }
